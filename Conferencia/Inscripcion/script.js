100033cad3c8b92de6c66e854f8ecefcf6559fab
unknown <fragusan2@gmail.com>
2022-07-16 06:18:27 -0300

    add modal interface login & singUp

---
+++ Conferencia/Inscripcion/script.js
@@ -0,0 +1,5 @@
+
+        function google() {
+            var myWindow = window.open("https://accounts.google.com/signin/v2/identifier?hl=es&flowName=GlifWebSignIn&flowEntry=ServiceLogin", "Cargando...", "width=360,height=450,left=600px,top=400px");
+        }
+    
