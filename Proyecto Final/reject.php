4c2525213782e6f332a875f70f85545a91159114
unknown <fragusan2@gmail.com>
2022-07-20 19:52:53 -0300

    reject y resolve añadidos

---
+++ Proyecto Final/reject.php
@@ -0,0 +1,4 @@
+<div class="alert alert-danger text-center mt-4">
+    <h4 class="fw-bolder fs-2">Ocurrio un error inesperado.</h4>
+    <span class="mt-2 fs-4">Intenta nuevamente. Si el problema persiste, contacta con el administrador</span>
+</div>
---
+++ Proyecto Final/resolve.php
@@ -0,0 +1,4 @@
+<div class="alert alert-success text-center mt-4">
+    <h4 class="fw-bolder fs-2">La operación se efectuo de manera satisfactoria.</h4>
+    <span class="mt-2 fs-4">En breve sera redirigido...</span>
+</div>
